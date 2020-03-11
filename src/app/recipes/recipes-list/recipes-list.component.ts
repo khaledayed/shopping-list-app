1043c9dd4185889b1feb55fb002061f59a5fc7f9
Khaled Ayed <ayed.khaled@gmail.com>
2020-03-11 21:31:11 +0100

    create recipes model

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
-
+recipes = [];
   constructor() { }
 
   ngOnInit() {
