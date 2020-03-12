5414439efae03a8db201e9c8019c03516a0561fe
Khaled Ayed <ayed.khaled@gmail.com>
2020-03-12 22:01:12 +0100

    create ingredient model and add shopping lis

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -9,6 +9,8 @@ import { Recipe } from '../recipes.model';
 export class RecipesListComponent implements OnInit {
 recipes: Recipe[] = [
   new Recipe('A Test Recipe', 'This is simply a test',
+  'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg' ),
+  new Recipe('A Test Recipe', 'This is simply a test',
   'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg' )
 ];
   constructor() { }
